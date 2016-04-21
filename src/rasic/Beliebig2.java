77844af1f07d72561ee1150d623e56c815338f84
Benjamin Rasic <brasic@student.tgm.ac.at>
2016-04-21 10:10:28 +0200

    InitialCommit

---
+++ src/rasic/Beliebig2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package rasic;
+
+/**
+ * @author Rasic Benjamin
+ * @version 1.0
+ * Random Text
+ */
+public class Beliebig2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
